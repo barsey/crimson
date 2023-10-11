5cb4c24b5330cdf28bb0778fb18966b1579db4fb
Barun Prasad <barun.prasad@hotmail.com>
2023-10-11 15:50:22 +0530

    integrate emotion

--- lib/components/Switch/index.tsx
+++ lib/components/Switch/index.tsx
@@ -1,6 +1,36 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
-import styles from './styles.module.scss';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  background: linear-gradient(250deg, #7b2ff7, #f107a3);
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  width: 48px;
+  height: 24px;
+  border-radius: 12px;
+`;
+
+const SwitchElement = styled.div<{ isOn: boolean }>`
+  width: 48px;
+  height: 20px;
+  background-color: rgba(255, 255, 255, 0.4);
+  display: flex;
+  justify-content: ${({ isOn }) => (isOn ? 'flex-end' : 'flex-start')};
+  border-radius: 10px;
+  padding: 2px;
+  cursor: pointer;
+`;
+
+const HandleElement = styled(motion.div)`
+  width: 20px;
+  height: 20px;
+  background-color: white;
+  border-radius: 10px;
+`;
 
 export function Switch() {
   const [isOn, setIsOn] = useState(false);
@@ -8,11 +38,11 @@ export function Switch() {
   const toggleSwitch = () => setIsOn(!isOn);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.switch} data-isOn={isOn} onClick={toggleSwitch}>
-        <motion.div className={styles.handle} layout transition={spring} />
-      </div>
-    </div>
+    <Container>
+      <SwitchElement isOn={isOn} onClick={toggleSwitch}>
+        <HandleElement layout transition={spring} />
+      </SwitchElement>
+    </Container>
   );
 }
 
