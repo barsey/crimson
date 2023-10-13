28a40a449f2019c692ed09437f4aa25718390598
Barun Prasad <barun.prasad@hotmail.com>
2023-10-13 18:11:23 +0530

    add storybook integration

---
+++ rollup.config.js
@@ -0,0 +1,63 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { babel } from '@rollup/plugin-babel';
+import { createRequire } from 'node:module';
+const requireFile = createRequire(import.meta.url);
+const packageJson = requireFile('./package.json');
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve({
+        extensions: ['.ts', '.tsx'],
+      }),
+      commonjs(),
+      typescript({
+        exclude: [
+          // Exclude test files
+          /\.test.((js|jsx|ts|tsx))$/,
+          // Exclude story files
+          /\.stories.((js|jsx|ts|tsx|mdx))$/,
+        ],
+        tsconfig: './tsconfig.json',
+      }),
+      terser(),
+      babel({
+        babelHelpers: 'runtime',
+        extensions: ['.ts', '.tsx'],
+        exclude: 'node_modules/**',
+      }),
+    ],
+    external: [
+      'react',
+      'react-dom',
+      '@emotion/react',
+      '@emotion/styled',
+      /@babel\/runtime/,
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'dist/types.d.ts', format: 'es' }],
+    plugins: [dts.default()],
+  },
+];
