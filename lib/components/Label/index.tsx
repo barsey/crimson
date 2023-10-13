7c7c531dfec26fcef29ee02f80fbf21333cfa52b
Barun Prasad <barun.prasad@hotmail.com>
2023-10-13 14:51:28 +0530

    remove old code

--- lib/components/Label/index.tsx
+++
@@ -1,6 +0,0 @@
-import styles from './styles.module.scss';
-
-export function Label(props: React.LabelHTMLAttributes<HTMLLabelElement>) {
-  const { className, ...otherProps } = props;
-  return <label className={`${className} ${styles.label}`} {...otherProps} />;
-}
--- lib/main.ts
+++
@@ -1,6 +0,0 @@
-import './global.scss';
-
-export { Button } from './components/Button';
-export { Input } from './components/Input';
-export { Label } from './components/Label';
-export { Switch } from './components/Switch';
