2c81261c50c7df4e8ec05e3ec9de02d55e15772a
Barun Prasad <barun.prasad@hotmail.com>
2024-06-25 14:24:01 +0530

    chore: update commitlint deps

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,1 @@
-exports.prompter = async (inquirerIns, commit) => {
-  (await import('@commitlint/cz-commitlint')).prompter(inquirerIns, commit);
-};
+module.exports = { extends: ['@commitlint/config-conventional'] };
